e4c706ebef3e9ffb1da496ef50eba783da64c825
马林 <11597313+lemC@user.noreply.gitee.com>
2023-03-16 09:21:32 +0800

    chore: upload base

--- src/pages/flower/index.tsx
+++ src/pages/flower/index.tsx
@@ -2,54 +2,85 @@
 // import { isWeChat, loadImage, getRandomTraits } from '@/utils';
 import { download } from '@/utils/download';
 import { PageContainer } from '@ant-design/pro-components';
-import { Button } from 'antd';
+import { Button, Upload, UploadProps } from 'antd';
+import { UploadChangeParam, UploadFile } from 'antd/es/upload';
 // import { Button, Col, message, Row, Tabs } from 'antd';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import styles from './index.less';
+import { beforeUpload, getBase64 } from './utils';
+import { LoadingOutlined, PlusOutlined } from '@ant-design/icons';
 
 const HomePage: React.FC = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
-  // // 选择的图片，用来拼接
-  // const [imgSelect, setImgSelect] = useState('');
+  // 选择的图片，用来拼接
+  const [imgSelect] = useState('');
+
+  // 用户头像
+  const [imageUrl, setImageUrl] = useState<string>();
+  const [loading, setLoading] = useState(false);
+
+  const handleChange: UploadProps['onChange'] = (
+    info: UploadChangeParam<UploadFile>,
+  ) => {
+    console.log('xxxx', info);
+    if (info.file.status === 'uploading') {
+      setLoading(true);
+      return;
+    }
+    if (info.file.status === 'done') {
+      // Get this url from response in real world.
+      getBase64(info.file.originFileObj!, (url) => {
+        setLoading(false);
+        setImageUrl(url);
+      });
+    }
+  };
+
+  const uploadButton = (
+    <div>
+      {loading ? <LoadingOutlined /> : <PlusOutlined />}
+      <div style={{ marginTop: 8 }}>Upload</div>
+    </div>
+  );
 
   // // 进行动态渲染
   useEffect(() => {
-    //   (async () => {
-    //     if (!canvasRef.current) {
-    //       return;
-    //     }
-    //     const ctx = canvasRef.current.getContext('2d');
-    //     if (!ctx) {
-    //       return;
-    //     }
-    //     const images = ['traits/Lian.jpg'];
-    //     ctx?.clearRect(0, 0, 200, 200);
-    //     ctx.beginPath();
-    //     ctx.rect(0, 0, 200, 200);
-    //     ctx.fillStyle = '#fff';
-    //     ctx.fill();
-    //     TAB_KEY_LIST.forEach((tab: keyImg) => {
-    //       let result = '';
-    //       const isSelect = imgSelect[tab];
-    //       if (isSelect) {
-    //         result =
-    //           TRAITS[tab].find((item) => item.key === imgSelect[tab])?.img || '';
-    //         images.push(result);
-    //       }
-    //     });
-    //     images.push(
-    //       images.splice(
-    //         images.findIndex((img) => img.includes('Faxing')),
-    //         1,
-    //       )[0],
-    //     );
-    //     const imagesObj = await Promise.all(images.map(loadImage));
-    //     imagesObj.forEach((item) => {
-    //       ctx?.drawImage(item, 0, 0, 200, 200);
-    //     });
-    //   })();
-  }, []);
+    (async () => {
+      if (!canvasRef.current) {
+        return;
+      }
+      const ctx = canvasRef.current.getContext('2d');
+      if (!ctx) {
+        return;
+      }
+      //     const images = ['traits/Lian.jpg'];
+      ctx?.clearRect(0, 0, 200, 200);
+      ctx.beginPath();
+      ctx.rect(0, 0, 200, 200);
+      ctx.fillStyle = '#fff';
+      ctx.fill();
+      //     TAB_KEY_LIST.forEach((tab: keyImg) => {
+      //       let result = '';
+      //       const isSelect = imgSelect[tab];
+      //       if (isSelect) {
+      //         result =
+      //           TRAITS[tab].find((item) => item.key === imgSelect[tab])?.img || '';
+      //         images.push(result);
+      //       }
+      //     });
+      //     images.push(
+      //       images.splice(
+      //         images.findIndex((img) => img.includes('Faxing')),
+      //         1,
+      //       )[0],
+      //     );
+      //     const imagesObj = await Promise.all(images.map(loadImage));
+      //     imagesObj.forEach((item) => {
+      //       ctx?.drawImage(item, 0, 0, 200, 200);
+      //     });
+    })();
+  }, [imgSelect]);
 
   // const onClickImg = (tab: keyImg, it: string) => {
   //   setImgSelect((per) => ({
@@ -109,6 +140,20 @@ const HomePage: React.FC = () => {
   const right = () => {
     return (
       <div className={styles.right}>
+        <Upload
+          name="avatar"
+          listType="picture-card"
+          className="avatar-uploader"
+          showUploadList={false}
+          beforeUpload={beforeUpload}
+          onChange={handleChange}
+        >
+          {imageUrl ? (
+            <img src={imageUrl} alt="avatar" style={{ width: '100%' }} />
+          ) : (
+            uploadButton
+          )}
+        </Upload>
         <div>
           <canvas id="canvas" ref={canvasRef} width="200" height="200"></canvas>
         </div>
---
+++ src/pages/flower/utils.ts
@@ -0,0 +1,20 @@
+import { message } from 'antd';
+import { RcFile } from 'antd/es/upload';
+
+export const getBase64 = (img: RcFile, callback: (url: string) => void) => {
+  const reader = new FileReader();
+  reader.addEventListener('load', () => callback(reader.result as string));
+  reader.readAsDataURL(img);
+};
+
+export const beforeUpload = (file: RcFile) => {
+  const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
+  if (!isJpgOrPng) {
+    message.error('You can only upload JPG/PNG file!');
+  }
+  const isLt2M = file.size / 1024 / 1024 < 2;
+  if (!isLt2M) {
+    message.error('Image must smaller than 2MB!');
+  }
+  return isJpgOrPng && isLt2M;
+};
