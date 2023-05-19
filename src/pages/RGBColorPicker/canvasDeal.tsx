0fda989933467f72536e46ff9f9399193e6b4918
lemma <lemma@tencent.com>
2023-05-19 18:01:36 +0800

    chore: 三色滤镜

--- src/pages/RGBColorPicker/canvasDeal.tsx
+++ src/pages/RGBColorPicker/canvasDeal.tsx
@@ -1,16 +1,19 @@
 import { PageContainer } from '@ant-design/pro-components';
-import { Col, Row, Input, Button } from 'antd';
+import { Col, Row, Input, Checkbox } from 'antd';
 import React, { useEffect, useRef, useState } from 'react';
 import styles from './styles.less';
 import { useLocalStorageState } from 'ahooks';
 import { getImageSize } from '@/utils/image';
 import { loadImage } from '@/utils';
-import { toRed } from '@/utils/imageUtil';
+import { toRed, toGreen, toBlue } from '@/utils/imageUtil';
+import type { CheckboxValueType } from 'antd/es/checkbox/Group';
 
 const LENA_PATH = 'https://upload.wikimedia.org/wikipedia/zh/3/34/Lenna.jpg';
 
 const OP = {
   redFilter: 'redFilter',
+  greenFilter: 'greenFilter',
+  blurFilter: 'blurFilter',
 };
 
 function ColorGrid() {
@@ -18,6 +21,8 @@ function ColorGrid() {
   const newCanvasRef = useRef<HTMLCanvasElement>(null);
 
   const [newData, setNewData] = useState<ImageData | null>();
+
+  const [oldData, setOldData] = useState<ImageData | null>();
 
   const [url, setUrl] = useLocalStorageState<string>('url', {
     defaultValue: LENA_PATH,
@@ -42,6 +47,7 @@ function ColorGrid() {
         ctx.drawImage(data, 0, 0, width, height);
 
         const imageData = ctx.getImageData(0, 0, width, height);
+        setOldData(imageData);
         setNewData(imageData);
       })
       .catch((error) => {
@@ -73,39 +79,70 @@ function ColorGrid() {
     setUrl(LENA_PATH);
   };
 
-  const handle = (op: keyof typeof OP) => {
-    const canvas = newCanvasRef.current;
-    if (!canvas || !url || !newData) {
+  const onChange = (checkedValues: CheckboxValueType[]) => {
+    console.log('checked = ', checkedValues);
+    const canvas = canvasRef.current;
+    if (!canvas || !url || !oldData) {
       return;
     }
     const ctx = canvas.getContext('2d');
     if (!ctx) {
       return;
     }
+    const { width, height } = oldData;
+    let nData: ImageData | null = oldData;
+    const imageData = ctx.getImageData(0, 0, width, height);
 
-    if (op === OP.redFilter) {
-      const imageData = ctx.getImageData(0, 0, newData.width, newData?.height);
-      const data = toRed(imageData);
-      setNewData(data);
+    if (checkedValues.includes(OP.redFilter)) {
+      nData = toRed(imageData);
     }
+
+    if (checkedValues.includes(OP.greenFilter)) {
+      nData = toGreen(imageData);
+    }
+
+    if (checkedValues.includes(OP.blurFilter)) {
+      nData = toBlue(imageData);
+    }
+    setNewData(nData);
   };
 
   return (
     <PageContainer>
       <div className={styles.container}>
         <Row>
-          <Col span={12}>
+          <Col span={12} className={styles.block__container}>
             <canvas ref={canvasRef} />
           </Col>
-          <Col span={12}>
+          <Col span={12} className={styles.block__container}>
             url:
             <Input value={url || LENA_PATH} onChange={handleChange} />
-            <Button onClick={() => handle(OP.redFilter)}>红色滤镜</Button>
+            <Checkbox.Group style={{ width: '100%' }} onChange={onChange}>
+              <Row>
+                <Col span={8}>
+                  <Checkbox value={OP.redFilter}>红色滤镜</Checkbox>
+                </Col>
+                <Col span={8}>
+                  <Checkbox value={OP.greenFilter}>绿色滤镜</Checkbox>
+                </Col>
+                <Col span={8}>
+                  <Checkbox value={OP.blurFilter}>蓝色滤镜</Checkbox>
+                </Col>
+                <Col span={8}>
+                  <Checkbox value="D">D</Checkbox>
+                </Col>
+                <Col span={8}>
+                  <Checkbox value="E">E</Checkbox>
+                </Col>
+              </Row>
+            </Checkbox.Group>
           </Col>
         </Row>
         <Row>
-          <Col span={12}>col-12</Col>
-          <Col span={12}>
+          <Col span={12} className={styles.block__container}>
+            col-12
+          </Col>
+          <Col span={12} className={styles.block__container}>
             <canvas ref={newCanvasRef} />
           </Col>
         </Row>
